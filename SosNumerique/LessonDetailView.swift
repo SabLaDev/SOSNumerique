8184d088b7bdaf4520468aa15fec5c1d97905e61
Sabrina AUGUSTY <sabrina.augusty@gmail.com>
2022-10-26 16:49:56 +0200

    Reposition du bouton favoris sur la vidéo de cours

--- SosNumerique/LessonDetailView.swift
+++ SosNumerique/LessonDetailView.swift
@@ -17,34 +17,37 @@ struct LessonDetailView: View {
     var body: some View {
         NavigationView{
             VStack {
-                
-                    ZStack{
-                        Image("img10").resizable().scaledToFit()
-                        
+                Image("img10")
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(
                         Button {islike.toggle()}label: {
-                                    if(islike == true){
-                                        Image(systemName: "heart.fill")
-                                            .foregroundColor(.red)
-                                    }else{
-                                        
-                                        Image(systemName: "heart")
-                                            
-                                            
+                                if(islike == true){
+                                    Image(systemName: "heart.fill")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.red)
+                                        .position(x:350,y:40)
+                                }else{
+                                    Image(systemName: "heart")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.white)
+                                        .position(x:350,y:40)
                                     }
-                                }
-                    
-                    }
-                
+                            })
+
                 List{
                     Section(header:Text("Nom du cours").font(.headline).foregroundColor(.black)){
-                        
-                           
+
                         }
 
                         Text("Description")
                     
                         Section(header:Text("Commentaire").font(.headline).foregroundColor(.black)){
                             Text("Commentaire")
+//                            List{
+//                                Text("réponse de commentaire")
+//                                Text("réponse de commentaire")
+//                            }
                             Text("Commentaire")
                             Text("Commentaire")
                             Text("Commentaire")
